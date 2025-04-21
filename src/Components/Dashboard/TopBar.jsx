b783e25c7c1f10e0b8e1620ab718119d4e69f688
Mark Neal <mark.neal22@gmail.com>
2025-04-21 00:35:52 -0500

    add motion npm and function to topbar button

--- src/Components/Dashboard/TopBar.jsx
+++ src/Components/Dashboard/TopBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import  { FiCalendar }  from  'react-icons/fi'
+import { motion } from "motion/react"
 
 function TopBar() {
   return (
@@ -9,9 +10,21 @@ function TopBar() {
                 <span className='text-sm font-bold block'>Goodmorning, Mark!</span>
                 <span className='text-xs block text-stone-500'>Tuesday, April 17th 2025</span>
             </div>
-            <button className='flex text-sm items-center gap-2 bg-stone-100 transition-colors hover:bg-violet-100 hover:text-violet-700 px-3 py-1.5 rounded'>
+            {/*<button className='flex text-sm items-center gap-2 bg-stone-100 transition-colors hover:bg-violet-100 hover:text-violet-700 px-3 py-1.5 rounded'>
                 <FiCalendar/><span> Prev 6 Months</span>
-            </button>
+            </button>*/}
+            <motion.button drag whileDrag={{ scale: 1.05, backgroundColor: "text-violet-700" }}
+              className='flex text-sm items-center gap-2 bg-stone-100 transition-colors hover:bg-violet-100 hover:text-violet-700 px-3 py-1.5 rounded'
+              whileHover={{
+                scale: 1.02,
+                transition: { duration: .5 },
+              }}
+              whileTap={{ scale: 0.9 }}
+            >
+              
+              <FiCalendar />
+              <span>Next 6 Months</span>
+             </motion.button>
         </div>
 
     </div>
