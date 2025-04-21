8d129d2533eefad1b82df4affb18a9cc6f14f62f
Mark Neal <mark.neal22@gmail.com>
2025-04-21 00:08:46 -0500

    Created StatCard components on the Dashboard

--- src/Components/Dashboard/TopBar.jsx
+++ src/Components/Dashboard/TopBar.jsx
@@ -11,7 +11,7 @@ function TopBar() {
             </div>
             <button className='flex text-sm items-center gap-2 bg-stone-100 transition-colors hover:bg-violet-100 hover:text-violet-700 px-3 py-1.5 rounded'>
                 <FiCalendar/><span> Prev 6 Months</span>
-                </button>
+            </button>
         </div>
 
     </div>
