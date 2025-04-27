58475386548b9da7446ace337f2d4da32c1f0984
Mark Neal <mark.neal22@gmail.com>
2025-04-27 13:56:59 -0500

    Updated expanded sidebar - WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
+import { SidebarItem } from './Components/Sidebar/SidebarExpanded'
+import './App.css'    
 import Sidebar from './Components/Sidebar/Sidebar'
 import Dashboard from './Components/Dashboard/Dashboard'
+import SidebarExpanded from './Components/Sidebar/SidebarExpanded'
+import { FiHome, FiBox, FiDollarSign, FiBarChart2, FiBook, FiSettings } from "react-icons/fi";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     
       <div className='grid gap-4 p-4 grid-cols-[200px_minmax(200px,_1fr)]'>
-        <Sidebar />
+        <SidebarExpanded>
+          <SidebarItem icon={<FiHome size={22} />} text="Dashboard" active />
+          <SidebarItem icon={<FiBarChart2 size={22} />} text="Statistics" />
+          <SidebarItem icon={<FiHome size={22} />} text="Inventory" />
+          <SidebarItem icon={<FiBox size={22} />} text="Orders" alert />
+          <SidebarItem icon={<FiDollarSign size={22} />} text="Billings" />
+          <hr className='my-3' />
+          <SidebarItem icon={<FiSettings size={22} />} text="Settings" />
+          <SidebarItem icon={<FiBook size={22} />} text="Resources" />
+
+        </SidebarExpanded>
         <Dashboard />
       </div>
 
--- src/Components/Sidebar/SidebarExpanded.jsx
+++ src/Components/Sidebar/SidebarExpanded.jsx
@@ -1,11 +1,94 @@
 import React from 'react'
+import { FiChevronsLeft, FiChevronsRight } from "react-icons/fi";
 
-function SidebarExpanded() {
+import { CgMoreVerticalAlt } from "react-icons/cg";
+import { useContext, createContext, useState } from 'react'
+
+const SidebarContext = createContext()
+
+export default function SidebarExpanded({ children }) {
+  const [ expanded, setExpanded ] = useState(true)
   return (
-    <div>
-      
-    </div>
+    <aside className='h-screen'>
+      <nav className='h-full flex flex-col gap-2'>
+        <div className='p-4 pb-2 flex justify-between items-center'>
+          <img 
+            src="./src/assets/logoipsum-custom-logo.svg" 
+            className={`overflow-hidden transition-all ${
+              expanded ? "w-32" : "w-0"
+            }`}
+            alt=""
+          />
+          <button onClick={() => setExpanded(curr =>!curr)} className='p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100'>
+            {expanded ? <FiChevronsLeft /> : <FiChevronsRight />}
+          </button>
+        </div>
+        <SidebarContext.Provider value={{ expanded }}>
+          <ul className='flex-1 px-3'>{children}</ul>
+        </SidebarContext.Provider>
+        <div className='border-t flex p-3'>
+          <img 
+            src="https://ui-avatars.com/api/?background=3B49DB&color=fff&bold=true" 
+            alt="Logo" 
+            className='w-10 h-10 rounded-md' 
+          />
+          <div className={`flex justify-between items-center overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}`}>
+            <div className='leading-4'>
+              <h4 className='font-semibold'>John Doe</h4>
+              <span className='text-xs text-stone-600'></span>
+            </div>
+            <CgMoreVerticalAlt size={20} />
+          </div>
+
+        </div>
+        
+      </nav>
+
+    </aside>
   )
 }
 
-export default SidebarExpanded
+export function SidebarItem({ icon, text, active, alert }) {
+  const { expanded } = useContext(SidebarContext)
+  return (
+    <li
+      className={`
+        relative flex items-center py-2 px-3 my-1
+        font-medium rounded-md cursor-pointer
+        transition-colors group
+        ${
+          active
+            ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+            : "hover:bg-indigo-50 text-gray-600"
+        }
+    `}
+    >
+      {icon}
+      <span className={`overflow-hidden transition-all ${
+              expanded ? "w-52 ml-3" : "w-0"
+      }`}
+      >
+      {text}
+      </span> 
+      {alert && (
+        <div className={`absolute right-2 w-2 h-2 rounded bg-color-snare-blue-400 
+          ${expanded ? "" : "top-2"
+          }`}
+        />
+      )}
+
+      {!expanded && (
+        <div
+          className={`absolute left-full rounded-md px-2 py-1 ml-6
+          bg-indigo-100 text-indigo-800 text-sm
+          invisible opacity-20 -translate-x-3 transition-all
+          group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
+      `}
+        >
+          {text}
+        </div>
+      )}
+    </li>
+  )
+}
+
