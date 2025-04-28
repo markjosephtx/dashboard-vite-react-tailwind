d0892285737b3a795020eb2e20e0324ad9af0a55
Mark Neal <mark.neal22@gmail.com>
2025-04-28 01:05:25 -0500

    Updated transform on the SidebarExpanded component

--- src/Components/Sidebar/SidebarExpanded.jsx
+++ src/Components/Sidebar/SidebarExpanded.jsx
@@ -10,12 +10,14 @@ const SidebarContext = createContext()
 export default function SidebarExpanded({ children }) {
   const [ expanded, setExpanded ] = useState(true)
   return (
-    <aside className={`lg:sticky top-4 lg:h-[calc(100vh-32px-48px)] grow-0`}>
+    <aside className={`lg:sticky top-4 lg:h-[calc(100vh-32px-48px)] grow-0 ${
+        expanded ? "h-auto" : "h-12"
+      }`}>
       <nav className='h-full flex flex-col gap-2'>
         <div className='p-4 pb-2 flex justify-between items-center'>
           <img 
             src={logo} 
-            className={`overflow-hidden transition-all 
+            className={`overflow-hidden transition-all duration-300 ease-[cubic-bezier(0,0.33,0,1)]
               ${expanded ? "lg:w-32 lg:h-auto w-32 h-auto" : "lg:w-0 lg:h-0 w-32 h-auto"}
             `}
             alt=""
@@ -26,15 +28,15 @@ export default function SidebarExpanded({ children }) {
         </div>
         
         <SidebarContext.Provider value={{ expanded }}>
-          <ul className='flex-1 px-3'>{children}</ul>
+          <ul className={`flex-1 px-3 transform transition-transform duration-300 ease-[cubic-bezier(0,0.33,0,1)] ${expanded ? "translate-x-0 opacity-100" : "translate-x-[-100%] lg:translate-x-0 opacity-0 lg:opacity-100" }`}>{children}</ul>
         </SidebarContext.Provider>
-        <div className='flex p-3'>
+        <div className={`flex p-3 transform transition-transform duration-300 ease-[cubic-bezier(0,0.33,0,1)] ${expanded ? "translate-x-0 opacity-100" : "translate-x-[-100%] lg:translate-x-0 opacity-0 lg:opacity-100" }`}>
           <img 
             src="https://ui-avatars.com/api/?background=3B49DB&color=fff&bold=true" 
             alt="Logo" 
             className='w-10 h-10 rounded-md' 
           />
-          <div className={`flex justify-between items-center overflow-hidden transition-all ${expanded ? "lg:w-full ml-3 w-full" : "lg:w-0 w-full lg:ml-0 ml-3"}`}>
+          <div className={`flex justify-between items-center overflow-hidden transition-all ease-[cubic-bezier(0,0.33,0,1)] ${expanded ? "lg:w-full ml-3 w-full" : "lg:w-0 w-full lg:ml-0 ml-3"}`}>
             <div className='leading-4'>
               <h4 className='font-semibold text-md'>John Doe</h4>
               <span className='text-xs text-stone-500'>john.doe@gmail.com</span>
@@ -73,7 +75,7 @@ export function SidebarItem({ icon, text, active, alert }) {
       {text}
       </span> 
       {alert && (
-        <div className={`absolute right-2 w-2 h-2 rounded bg-snare-blue-600
+        <div className={`animate-ping absolute right-2 w-2 h-2 rounded bg-snare-blue-600
           ${expanded ? "" : "lg:top-2"
           }`}
         />
