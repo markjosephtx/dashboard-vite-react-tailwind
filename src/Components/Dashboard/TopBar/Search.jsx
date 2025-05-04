4b1f516e218928b3455e1c0726a7130bbff0c8a6
Mark Neal <mark.neal22@gmail.com>
2025-05-03 21:34:32 -0500

    Fixed search function

--- src/Components/Dashboard/TopBar/Search.jsx
+++ src/Components/Dashboard/TopBar/Search.jsx
@@ -1,40 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FiSearch, FiCommand } from "react-icons/fi";
 import CommandMenu from "./CommandMenu";
 
-function Search() {
-  const [open, setOpen] = useState(false);
+  
+  const Search = () => {
+    const [searchOpen, setSearchOpen] = useState(false);
+  
+    const handleSearchToggle = () => {
+      setSearchOpen(!searchOpen);
+    };
+  
+    const handleSearchClose = () => {
+      setSearchOpen(false);
+    };
 
-  const handleInputFocus = () => {
-    setOpen(true); // Open the CommandMenu when the input is focused
-  };
-
-  const handleInputBlur = (e) => {
-    // Delay closing to allow interaction with the CommandMenu
-    setTimeout(() => {
-      if (!e.currentTarget.contains(document.activeElement)) {
-        setOpen(false);
+    useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.metaKey && event.key === 'k') {
+        handleSearchToggle();
       }
-    }, 150);
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleSearchToggle]);
+  
+    return (
+      <div>
+        <div className="bg-[#F1F3F6] w-full lg:w-96 mb-4 relative rounded flex items-center px-4 py-2 text-sm">
+          <FiSearch className="mr-2" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full bg-transparent placeholder:text-stone-400 focus:outline-none"
+            onClick={handleSearchToggle} // Open CommandMenu on focus
+          />
+          <span className="p-1 text-xs flex gap-0.5 items-center shadow bg-stone-50 rounded absolute right-1.5 top-1/2 -translate-y-1/2">
+            <FiCommand />K
+          </span>
+        </div>
+        {searchOpen && <CommandMenu open={searchOpen} setOpen={setSearchOpen} />}
+      </div>
+    );
   };
 
-  return (
-    <div onBlur={handleInputBlur}>
-      <div className="bg-[#F1F3F6] w-full lg:w-96 mb-4 relative rounded flex items-center px-4 py-2 text-sm">
-        <FiSearch className="mr-2" />
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-full bg-transparent placeholder:text-stone-400 focus:outline-none"
-          onFocus={handleInputFocus} // Open CommandMenu on focus
-        />
-        <span className="p-1 text-xs flex gap-0.5 items-center shadow bg-stone-50 rounded absolute right-1.5 top-1/2 -translate-y-1/2">
-          <FiCommand />K
-        </span>
-      </div>
-      {open && <CommandMenu open={open} setOpen={setOpen} />}
-    </div>
-  );
-}
 
 export default Search;
