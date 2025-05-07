2890a5c26e10fa42ef2ee2e2b875d63856f98e0d
Mark Neal <mark.neal22@gmail.com>
2025-05-06 23:14:41 -0500

    Added light and dark mode image toggle

---
+++ src/Components/DarkModeToggleImage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useDarkMode } from "../context/ThemeContext";
+import lightModeImage from '../assets/light-mode-image.png';
+import darkModeImage from '../assets/dark-mode-image.png';
+
+const DarkModeToggleImage = () => {
+  const { darkMode, setDarkMode } = useDarkMode();
+
+  return (
+    <div className="flex items-center gap-4">
+      {/* Light Mode Image */}
+      <img
+        src={lightModeImage}
+        alt="Light Mode"
+        className={`w-[350px] h-[245px] cursor-pointer ${
+          !darkMode ? "opacity-100" : "opacity-100"
+        }`}
+        onClick={() => setDarkMode(false)}
+      />
+
+      {/* Dark Mode Image */}
+      <img
+        src={darkModeImage}
+        alt="Dark Mode"
+        className={`w-[350px] h-[245px] cursor-pointer ${
+          darkMode ? "opacity-100" : "opacity-100"
+        }`}
+        onClick={() => setDarkMode(true)}
+      />
+    </div>
+  );
+};
+
+export default DarkModeToggleImage;
--- src/Components/Settings/AppearanceSection.jsx
+++ src/Components/Settings/AppearanceSection.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import DarkModeToggler from '../DarkModeToggler'
+import DarkModeToggleImage from '../DarkModeToggleImage'
 
 
 function AppearanceSection() {
   return (
     <>
       
-      <div>
+      <div className='w-full flex flex-col gap-6'>
         <div className='border-b border-neutral-200 dark:border-neutral-700 pb-6'>
           <h2 className='text-2xl font-bold text-neutral-950 dark:text-white'>Appearance</h2>
           <p className='text-neutral-500 dark:text-neutral-300'>Customize the appearance of the app. Automatically switch between day and night themes.</p>
         </div>
-        <DarkModeToggler />
+        <div className=' '>
+          <h3 className='text-lg font-semibold text-neutral-950 dark:text-white'>Theme</h3>
+          <p className='text-neutral-500 dark:text-neutral-300'>Select the theme for the dashboard.</p>
+        </div>
+        <DarkModeToggleImage/>
+        
       </div>
     
     
